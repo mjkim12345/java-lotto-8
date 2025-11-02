52c9b0e735ad0186f591ac51579e0d89ed6cec4d
user <offonoff6101@naver.com>
2025-11-02 15:03:20 +0900

    Feat(LottoService): Add generateRandomLottos method to create multiple random lottos

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -2,7 +2,9 @@ package lotto.service;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import lotto.domain.Lotto;
+import lotto.domain.Lottos;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class LottoService {
@@ -10,5 +12,14 @@ public class LottoService {
     public Lotto generateNumbers() {
         List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
         return Lotto.of(numbers);
+    }
+
+    public Lottos generateRandomLottos(int count) {
+        List<Lotto> lotto = new ArrayList<>();
+
+        for (int i = 0; i < count; i++) {
+            lotto.add(generateNumbers());
+        }
+        return Lottos.of(lotto);
     }
 }
