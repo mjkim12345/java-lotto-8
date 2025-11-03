0724f3192e47dca0f30ae09c39acc47d87c43f68
user <offonoff6101@naver.com>
2025-11-03 19:31:59 +0900

    Feat(OutputView): Implement printStatistics() in OutputView to display winning results

--- src/main/java/lotto/WinningRank.java
+++ src/main/java/lotto/WinningRank.java
@@ -4,7 +4,8 @@ public enum WinningRank {
     FIRST(2000000000),
     SECOND(30000000),
     THIRD(1500000),
-    FOURTH(5000),
+    FOURTH(50000),
+    FIFTH(5000),
     NONE(0);
 
     private int winningAmount;
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,6 +1,8 @@
 package lotto.view;
 
+import lotto.WinningRank;
 import lotto.domain.Lotto;
+import lotto.domain.LottoStatistics;
 import lotto.domain.Lottos;
 
 import java.util.List;
@@ -8,6 +10,12 @@ import java.util.List;
 public class OutputView {
 
     private static final String LOTTO_AMOUNT_MESSAGE = "%s개를 구매했습니다.";
+    private static final String WINNING_STATISTICS = "당첨 통계\n---";
+    private static final String FIFTH_MESSAGE = "3개 일치 (%s원) - %s개";
+    private static final String FOURTH_MESSAGE = "4개 일치 (%s원) - %d개";
+    private static final String THIRD_MESSAGE = "5개 일치 (%s원) - %d개";
+    private static final String SECOND_MESSAGE = "5개 일치, 보너스 볼 일치 (%s원) - %d개";
+    private static final String FIRST_MESSAGE = "6개 일치 (%s원) - %d개";
 
     public static void printLottoAmount(int lottoAmount) {
         System.out.printf(LOTTO_AMOUNT_MESSAGE, lottoAmount);
@@ -18,5 +26,14 @@ public class OutputView {
         for (Lotto lotto1 : lotto) {
             System.out.println(lotto1.sortedNumbers());
         }
+    }
+
+    public static void printStatistics(LottoStatistics lottoStatistics) {
+        System.out.printf(WINNING_STATISTICS);
+        System.out.printf(FIFTH_MESSAGE, WinningRank.FIFTH, lottoStatistics.getWinningAmount(WinningRank.FIFTH));
+        System.out.printf(FOURTH_MESSAGE, WinningRank.FOURTH, lottoStatistics.getWinningAmount(WinningRank.FOURTH));
+        System.out.printf(THIRD_MESSAGE, WinningRank.THIRD, lottoStatistics.getWinningAmount(WinningRank.THIRD));
+        System.out.printf(SECOND_MESSAGE, WinningRank.SECOND, lottoStatistics.getWinningAmount(WinningRank.SECOND));
+        System.out.printf(FIRST_MESSAGE, WinningRank.FIRST, lottoStatistics.getWinningAmount(WinningRank.FIRST));
     }
 }
